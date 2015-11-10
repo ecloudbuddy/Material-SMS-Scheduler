40849325b928b52d4aaedc1fe0089b232066ed58
Kyle Szombathy <kylemail2003@gmail.com>
2015-11-09 18:35:32 -0800

    Updated to android tutorial steps
    Did android tutorial

--- app/src/main/java/com/kyleszombathy/sms_scheduler/Home.java
+++ app/src/main/java/com/kyleszombathy/sms_scheduler/Home.java
@@ -1,5 +1,6 @@
 package com.kyleszombathy.sms_scheduler;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class Home extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.kyleszombathy.sms_scheduler.MESSAGE";
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,3 +62,5 @@ public class Home extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
