722ac178a10b5cc5dcdd012dff3bbba4803733bf
Kyle Szombathy <kylemail2003@gmail.com>
2015-11-18 00:56:29 -0800

    More work on contacts search

--- app/src/main/java/com/kyleszombathy/sms_scheduler/AddMessage.java
+++ app/src/main/java/com/kyleszombathy/sms_scheduler/AddMessage.java
@@ -1,8 +1,11 @@
 package com.kyleszombathy.sms_scheduler;
 
-import android.content.ContentProvider;
+import android.app.FragmentTransaction;
+import android.app.LoaderManager;
 import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -10,15 +13,24 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 
-public class AddMessage extends AppCompatActivity
+public class AddMessage extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>
 {
     static final int PICK_CONTACT_REQUEST = 1;
+    private static final String[] PROJECTION =
+            {
+                    ContactsContract.Data._ID,
+                    ContactsContract.CommonDataKinds.Identity.IDENTITY,
+                    ContactsContract.CommonDataKinds.Phone.NUMBER,
+                    ContactsContract.CommonDataKinds.Photo.PHOTO
+            };
+    private static final String SELECTION = ContactsContract.Data.LOOKUP_KEY + " = ?";
+    String mSelectionClause = null;
+    private String[] mSelectionArgs = { "" };
+    private String mLookupKey;
     EditText et;
 
     @Override
@@ -38,7 +50,7 @@ public class AddMessage extends AppCompatActivity
         // Setting editText field
         et =(EditText)findViewById(R.id.phoneNumber);
         // TODO: Doesn't work yet, works only from button
-        et.setOnKeyListener(new View.OnKeyListener() {
+/*        et.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (event.getAction()==KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
@@ -50,7 +62,7 @@ public class AddMessage extends AppCompatActivity
                 }
                 return false;
             }
-        });
+        });*/
     }
 
     public void showTimePickerDialog(View v) {
@@ -61,6 +73,12 @@ public class AddMessage extends AppCompatActivity
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void showContactsFragment(View v) {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment2, new ContactsFragment());
+        ft.commit();
     }
 
     public void openContactSearch(View v) {
@@ -75,19 +93,34 @@ public class AddMessage extends AppCompatActivity
             if (resultCode == RESULT_OK) {
                 // A contact was picked.
                 Uri contactUri = data.getData();
+                System.out.println(contactUri);
                 String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
                 // TODO: CurorLoader Implementation https://developer.android.com/training/basics/intents/result.html
-                CursorLoader loader = new CursorLoader(ContentProvider, contactUri, projection, null, null, null);
-/*                Cursor cursor = getContentResolver()
-                        .query(contactUri, projection, null, null, null);
-                cursor.moveToFirst();*/
+                CursorLoader loader = new CursorLoader(getApplicationContext(), contactUri, projection, null, null, null);
 
-                String contactName = data.getStringExtra("android.intent.extra.shortcut.NAME");
+
+                System.out.println(loader.toString());
+
+/*                String contactName = data.getStringExtra("android.intent.extra.shortcut.NAME");
                 System.out.println(contactUri);
-                et.setText(contactName, TextView.BufferType.EDITABLE);
+                et.setText(contactName, TextView.BufferType.EDITABLE);*/
             }
         }
     }
 
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
--- app/src/main/java/com/kyleszombathy/sms_scheduler/ContactsFragment.java
+++ app/src/main/java/com/kyleszombathy/sms_scheduler/ContactsFragment.java
@@ -1,161 +1,197 @@
 package com.kyleszombathy.sms_scheduler;
 
 
-import android.annotation.SuppressLint;
+import android.app.ListFragment;
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract.Contacts;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
-import android.view.LayoutInflater;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.SimpleCursorAdapter;
 
 /**
  * TODO: File still doesn't work, tutorial not completed. Tried to use tutorial, then switched halway Contact Provider implementation
- * Accomidates with 2 xml files, contacts_list_item.xml and contacts_list_view.xml
  * Following guide from https://developer.android.com/training/contacts-provider/retrieve-names.html
  */
-public class ContactsFragment extends Fragment implements
+public class ContactsFragment extends ListFragment implements
         LoaderManager.LoaderCallbacks<Cursor>,
-        AdapterView.OnItemClickListener {
-    /*DEFINE GLOBAL VARIABLES*/
-    /*
-     * Defines an array that contains column names to move from
-     * the Cursor to the ListView.
-     */
-    @SuppressLint("InlinedApi")
-    private final static String[] FROM_COLUMNS = {
-            Build.VERSION.SDK_INT
-                    >= Build.VERSION_CODES.HONEYCOMB ?
-                    Contacts.DISPLAY_NAME_PRIMARY :
-                    Contacts.DISPLAY_NAME
-    };
-    /*
-     * Defines an array that contains resource ids for the layout views
-     * that get the Cursor column contents. The id is pre-defined in
-     * the Android framework, so it is prefaced with "android.R.id"
-     */
-    private final static int[] TO_IDS = {
-            android.R.id.text1
-    };
-    // Define global mutable variables
-    // Define a ListView object
-    ListView mContactsList;
-    // Define variables for the contact the user selects
-    // The contact's _ID value
-    long mContactId;
-    // The contact's LOOKUP_KEY
-    String mContactKey;
-    // A content URI for the selected contact
-    Uri mContactUri;
-    // An adapter that binds the result Cursor to the ListView
-    private SimpleCursorAdapter mCursorAdapter;
-    // Columns to return from query
-    @SuppressLint("InlinedApi")
-    private static final String[] PROJECTION =
-            {
-                    Contacts._ID,
-                    Contacts.LOOKUP_KEY,
-                    Build.VERSION.SDK_INT
-                            >= Build.VERSION_CODES.HONEYCOMB ?
-                            Contacts.DISPLAY_NAME_PRIMARY :
-                            Contacts.DISPLAY_NAME
+        AdapterView.OnItemClickListener
+{
+    // This is the Adapter being used to display the list's data.
+    SimpleCursorAdapter mAdapter;
 
-            };
-    /*DEFINE CONSTANTS FOR THE CURSOR COLUMN INDEXES*/
-    // The column index for the _ID column
-    private static final int CONTACT_ID_INDEX = 0;
-    // The column index for the LOOKUP_KEY column
-    private static final int LOOKUP_KEY_INDEX = 1;
-    /* SPECIFYING THE SELECTION CRITERIA*/
-    // Defines the text expression
-    @SuppressLint("InlinedApi")
-    private static final String SELECTION =
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
-                    Contacts.DISPLAY_NAME_PRIMARY + " LIKE ?" :
-                    Contacts.DISPLAY_NAME + " LIKE ?";
-    // Defines a variable for the search string
-    private String mSearchString;
-    // Defines the array to hold values that replace the ?
-    private String[] mSelectionArgs = { mSearchString };
+    // The SearchView for doing filtering.
+    SearchView mSearchView;
+
+    // If non-null, this is the current filter the user has provided.
+    String mCurFilter;
 
 
-
-    public ContactsFragment() {
-        // Required empty public constructor
-    }
-
-    // A UI Fragment must inflate its View
-    @Override
+/*    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the fragment layout
-        return inflater.inflate(R.layout.contacts_list_view,
-                container, false);
-    }
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
+    }*/
 
-    public void onActivityCreated(Bundle savedInstanceState) {
+
+    @Override public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        // Gets the ListView from the View list of the parent activity
-        mContactsList = (ListView) getActivity().findViewById(android.R.id.list);
-        //System.out.println(getActivity());
-        // Gets a CursorAdapter
-        mCursorAdapter = new SimpleCursorAdapter(
-                getActivity(),
-                R.layout.contacts_list_item,
-                null,
-                FROM_COLUMNS, TO_IDS,
-                0);
-        // Sets the adapter for the ListView
-        mContactsList.setAdapter(mCursorAdapter);
-        // Set the item click listener to be the current fragment.
-        mContactsList.setOnItemClickListener(this);
-        // Initializes the loader
+        // Give some text to display if there is no data.  In a real
+        // application this would come from a resource.
+        setEmptyText("No phone numbers");
+
+        // We have a menu item to show in action bar.
+        setHasOptionsMenu(true);
+
+        // Create an empty adapter we will use to display the loaded data.
+        mAdapter = new SimpleCursorAdapter(getActivity(),
+                android.R.layout.simple_list_item_2, null,
+                new String[] { ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts.CONTACT_STATUS },
+                new int[] { android.R.id.text1, android.R.id.text2 }, 0);
+        setListAdapter(mAdapter);
+
+        // Start out with a progress indicator.
+        setListShown(false);
+
+        // Prepare the loader.  Either re-connect with an existing one,
+        // or start a new one.
         getLoaderManager().initLoader(0, null, this);
     }
 
-
     @Override
-    public android.support.v4.content.Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
-    }
-
-    @Override
-    public void onLoadFinished(android.support.v4.content.Loader<Cursor> loader, Cursor data) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
     }
 
-    @Override
-    public void onLoaderReset(android.support.v4.content.Loader<Cursor> loader) {
+    public static class MySearchView extends SearchView {
+        public MySearchView(Context context) {
+            super(context);
+        }
 
+        // The normal SearchView doesn't clear its search text when
+        // collapsed, so we will do this for it.
+        @Override
+        public void onActionViewCollapsed() {
+            setQuery("", false);
+            super.onActionViewCollapsed();
+        }
+    }
+
+    @Override public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Place an action bar item for searching.
+        MenuItem item = menu.add("Search");
+        item.setIcon(android.R.drawable.ic_menu_search);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM
+                | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+        mSearchView = new MySearchView(getActivity());
+        mSearchView.setOnQueryTextListener((SearchView.OnQueryTextListener) this);
+        mSearchView.setOnCloseListener((SearchView.OnCloseListener) this);
+        mSearchView.setIconifiedByDefault(true);
+        item.setActionView(mSearchView);
+    }
+
+    public boolean onQueryTextChange(String newText) {
+        // Called when the action bar search text has changed.  Update
+        // the search filter, and restart the loader to do a new query
+        // with this filter.
+        String newFilter = !TextUtils.isEmpty(newText) ? newText : null;
+        // Don't do anything if the filter hasn't actually changed.
+        // Prevents restarting the loader when restoring state.
+        if (mCurFilter == null && newFilter == null) {
+            return true;
+        }
+        if (mCurFilter != null && mCurFilter.equals(newFilter)) {
+            return true;
+        }
+        mCurFilter = newFilter;
+        getLoaderManager().restartLoader(0, null, this);
+        return true;
+    }
+
+    public boolean onQueryTextSubmit(String query) {
+        // Don't care about this.
+        return true;
+    }
+
+
+    public boolean onClose() {
+        if (!TextUtils.isEmpty(mSearchView.getQuery())) {
+            mSearchView.setQuery(null, true);
+        }
+        return true;
+    }
+
+    @Override public void onListItemClick(ListView l, View v, int position, long id) {
+        // Insert desired behavior here.
+        Log.i("FragmentComplexList", "Item clicked: " + id);
+    }
+
+    // These are the Contacts rows that we will retrieve.
+    static final String[] CONTACTS_SUMMARY_PROJECTION = new String[] {
+            ContactsContract.Contacts._ID,
+            ContactsContract.Contacts.DISPLAY_NAME,
+            ContactsContract.Contacts.CONTACT_STATUS,
+            ContactsContract.Contacts.CONTACT_PRESENCE,
+            ContactsContract.Contacts.PHOTO_ID,
+            ContactsContract.Contacts.LOOKUP_KEY,
+    };
+
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // This is called when a new Loader needs to be created.  This
+        // sample only has one Loader, so we don't care about the ID.
+        // First, pick the base URI to use depending on whether we are
+        // currently filtering.
+        Uri baseUri;
+        if (mCurFilter != null) {
+            baseUri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_FILTER_URI,
+                    Uri.encode(mCurFilter));
+        } else {
+            baseUri = ContactsContract.Contacts.CONTENT_URI;
+        }
+
+        // Now create and return a CursorLoader that will take care of
+        // creating a Cursor for the data being displayed.
+        String select = "((" + ContactsContract.Contacts.DISPLAY_NAME + " NOTNULL) AND ("
+                + ContactsContract.Contacts.HAS_PHONE_NUMBER + "=1) AND ("
+                + ContactsContract.Contacts.DISPLAY_NAME + " != '' ))";
+        return new CursorLoader(getActivity(), baseUri,
+                CONTACTS_SUMMARY_PROJECTION, select, null,
+                ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC");
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View item, int position, long rowID) {
-        //TODO: Disabled because doesn't work yet
-/*        // Get cursor adapter object
-        CursorAdapter adapter=(CursorAdapter)parent.getAdapter();
-        // Get the Cursor
-        Cursor cursor = adapter.getCursor();
-        // Move to the selected contact
-        cursor.moveToPosition(position);
-        // Get the _ID value
-        mContactId = getId(CONTACT_ID_INDEX);
-        // Get the selected LOOKUP KEY
-        mContactKey = getString(CONTACT_KEY_INDEX);
-        // Create the contact's content Uri
-        mContactUri = Contacts.getLookupUri(mContactId, mContactKey);*/
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Swap the new cursor in.  (The framework will take care of closing the
+        // old cursor once we return.)
+        mAdapter.swapCursor(data);
 
-        /*
-         * You can use mContactUri as the content URI for retrieving
-         * the details for a contact.*/
+        // The list should now be shown.
+        if (isResumed()) {
+            setListShown(true);
+        } else {
+            setListShownNoAnimation(true);
+        }
+    }
+
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // This is called when the last Cursor provided to onLoadFinished()
+        // above is about to be closed.  We need to make sure we are no
+        // longer using it.
+        mAdapter.swapCursor(null);
     }
 
 }
